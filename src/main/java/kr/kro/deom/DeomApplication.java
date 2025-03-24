61971698341654f44102419aa0d73c8d8cc04540
JunhyunKang <kangjunhyun38@gmail.com>
2025-03-24 16:05:27 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/kr/kro/deom/DeomApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.deom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/kro/deom/DeomApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.deom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
