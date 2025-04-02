a42b31fb4e94f8f64d3de72702f2f597a3ad83e4
codmsrose <97442570+codmsrose@users.noreply.github.com>
2025-04-02 22:48:07 +0900

    feat: kakao, naver 유저 정보
    ISSUE: #8

---
+++ src/main/java/kr/kro/deom/common/security/oauth/NaverUserInfo.java
@@ -0,0 +1,32 @@
+package kr.kro.deom.common.security.oauth;
+
+import java.util.Map;
+
+public class NaverUserInfo implements OAuth2UserInfo {
+
+  private final Map<String, Object> response;
+
+  public NaverUserInfo(Map<String, Object> attributes) {
+    this.response = (Map<String, Object>) attributes.get("response");
+  }
+
+  @Override
+  public OAuth2Provider getProvider() {
+    return OAuth2Provider.NAVER;
+  }
+
+  @Override
+  public String getProviderId() {
+    return response.get("id").toString();
+  }
+
+  @Override
+  public String getEmail() {
+    return response.get("email").toString();
+  }
+
+  @Override
+  public String getName() {
+    return response.get("name").toString();
+  }
+}
