fde0497614e0e981e8797bd6ef79b2f50d6cf8c1
gyuri127 <gyurih127@gmail.com>
2025-04-04 11:30:44 +0900

    feat : deom entity, repository 생성
    issue : #33

---
+++ src/main/java/kr/kro/deom/domain/deom/entity/Deom.java
@@ -0,0 +1,38 @@
+package kr.kro.deom.domain.deom.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "service")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Deom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "store_id", nullable = false)
+    private Long storeId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "required_stamp_amount", nullable = false)
+    private Integer requiredStampAmount;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
---
+++ src/main/java/kr/kro/deom/domain/deom/repository/DeomRepository.java
@@ -0,0 +1,7 @@
+package kr.kro.deom.domain.deom.repository;
+
+import kr.kro.deom.domain.deom.entity.Deom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeomRepository extends JpaRepository<Deom, Long> {
+}
