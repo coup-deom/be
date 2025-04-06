359304e80142558deb80ef5d4c65300050d5b4b4
gyuri127 <gyurih127@gmail.com>
2025-04-07 01:13:36 +0900

    feat : otp 조회 test 코드
    - otp 조회 test 코드
    - OtpVerifyResponse에 @getter 추가
    
    issue : #40

--- src/main/java/kr/kro/deom/domain/otp/dto/OtpVerifyResponse.java
+++ src/main/java/kr/kro/deom/domain/otp/dto/OtpVerifyResponse.java
@@ -3,7 +3,9 @@ package kr.kro.deom.domain.otp.dto;
 import kr.kro.deom.domain.otp.entity.OtpStatus;
 import kr.kro.deom.domain.otp.entity.OtpType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class OtpVerifyResponse {
   private Long otpId;
