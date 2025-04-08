65f1c65ba0269181d99c0cf01ababbeae3e84942
gyuri127 <gyurih127@gmail.com>
2025-04-08 17:03:45 +0900

    feat : 사장 otp 입력 otpVerify 주석 해제 및 코드 변경
    - db에서 otpUsage 가져오는 부분 변경
    - storeRepository findByOwnerId 추가
    - OtpRepository에 findIDByOtpAndStoreIdAndStatus,findOtpInfoByOtpAndStoreIdAndStatus 추가
    
    issue : #42

--- src/main/java/kr/kro/deom/domain/otp/repository/OtpRepository.java
+++ src/main/java/kr/kro/deom/domain/otp/repository/OtpRepository.java
@@ -1,7 +1,10 @@
 package kr.kro.deom.domain.otp.repository;
 
+import java.util.Map;
+import java.util.Optional;
 import kr.kro.deom.domain.otp.entity.OtpStatus;
 import kr.kro.deom.domain.otp.entity.OtpUsage;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,13 @@ public interface OtpRepository extends CrudRepository<OtpUsage, Long> {
     OtpUsage findByOtpAndStoreIdAndStatus(Long otp, Long storeId, OtpStatus status);
 
     boolean existsByOtpAndStoreIdAndStatus(Long otp, Long storeId, OtpStatus status);
+
+    @Query(
+            "SELECT o.id FROM OtpUsage o WHERE o.otp = :otp AND o.storeId =:storedId AND o.status =:status")
+    Optional<Long> findIDByOtpAndStoreIdAndStatus(Long otp, Long storeId, OtpStatus status);
+
+    @Query(
+            "SELECT new map(o.id as id, o.type as type, o.status as status) FROM OtpUsage o WHERE o.otp =:otp AND o.storeId =:storedId AND o.status =:status")
+    Optional<Map<String, Object>> findOtpInfoByOtpAndStoreIdAndStatus(
+            Long otp, Long storeId, OtpStatus status);
 }
