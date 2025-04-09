0825716b7d9a3b2f7f7e59e9a4fc00eb57b638da
gyuri127 <gyurih127@gmail.com>
2025-04-09 10:36:26 +0900

    feat : otp 입력 api에 swagger annotation 추가
    issue : #42

--- src/main/java/kr/kro/deom/domain/otp/controller/OtpVerityController.java
+++ src/main/java/kr/kro/deom/domain/otp/controller/OtpVerityController.java
@@ -1,5 +1,7 @@
 package kr.kro.deom.domain.otp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.kro.deom.common.response.ApiResponse;
 import kr.kro.deom.common.response.CommonSuccessCode;
@@ -16,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/otp")
 @RequiredArgsConstructor
+@Tag(name = "OTP 입력 처리", description = "사장님 OTP 입력 API")
 public class OtpVerityController {
 
     private final OtpOwnerServiceImpl otpOwnerService;
 
+    @Operation(summary = "OTP 입력", description = "사장이 OTP를 입력합니다.")
     @PostMapping("/verify")
     public ResponseEntity<ApiResponse<OtpVerifyResponse>> verifyOtp(
             @RequestBody @Valid OtpVerifyRequest request) {
