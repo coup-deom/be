3320324a98f2506d8f35b443439d42fa315b5241
codmsrose <97442570+codmsrose@users.noreply.github.com>
2025-05-08 13:27:47 +0900

    feat: if문 return 추가

--- src/main/java/kr/kro/deom/common/security/handler/OAuth2SuccessHandler.java
+++ src/main/java/kr/kro/deom/common/security/handler/OAuth2SuccessHandler.java
@@ -30,6 +30,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         if (oAuth2User.getRole() == Role.PENDING) {
             String targetUrl = "http://localhost:5173/user/role?userId=" + oAuth2User.getId();
             getRedirectStrategy().sendRedirect(request, response, targetUrl);
+            return;
         }
 
         String accessToken = jwtUtil.createAccessToken(oAuth2User.getId(), oAuth2User.getRole());
