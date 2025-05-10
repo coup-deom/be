bb9ffd239814a5b86ca60809aa7e53188a67233b
JunhyunKang <kangjunhyun38@gmail.com>
2025-05-10 18:27:36 +0900

    refactor: 어노테이션으로 변경

--- src/main/java/kr/kro/deom/domain/myStamp/dto/UserAccumulatedStampsDto.java
+++ src/main/java/kr/kro/deom/domain/myStamp/dto/UserAccumulatedStampsDto.java
@@ -1,11 +1,15 @@
 package kr.kro.deom.domain.myStamp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserAccumulatedStampsDto {
     private Long userId;
     private Integer accumulatedStampAmount;
-
-    public UserAccumulatedStampsDto(Long userId, Integer accumulatedStampAmount) {
-        this.userId = userId;
-        this.accumulatedStampAmount = accumulatedStampAmount;
-    }
 }
