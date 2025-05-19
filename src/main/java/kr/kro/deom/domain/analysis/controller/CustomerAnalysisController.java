5a4daca2a3b42266c4e467b1933191281dcbba5f
JunhyunKang <kangjunhyun38@gmail.com>
2025-05-19 15:30:59 +0900

    fix: api명 수정

--- src/main/java/kr/kro/deom/domain/analysis/controller/CustomerAnalysisController.java
+++ src/main/java/kr/kro/deom/domain/analysis/controller/CustomerAnalysisController.java
@@ -34,7 +34,7 @@ public class CustomerAnalysisController {
     }
 
     @Operation(summary = "내 가게의 스탬프 거래 조회 ", description = "내 가게의 스탬프 거래를 조회합니다.")
-    @GetMapping("/recent-coupons")
+    @GetMapping("/recent-exchanges")
     public ResponseEntity<ApiResponse<List<StampExchangeResponse>>> getRecentExchanges(
             @RequestParam Long storeId) {
         List<StampExchangeResponse> responses =
