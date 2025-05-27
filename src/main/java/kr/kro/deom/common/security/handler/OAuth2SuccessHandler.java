4ccfb5e70df9d11d494d8a6987cb098a214d58fa
gyuri127 <gyurih127@gmail.com>
2025-05-28 00:28:25 +0900

    fix : REDIRECT_UR 수정 issue : #98

--- src/main/java/kr/kro/deom/common/security/handler/OAuth2SuccessHandler.java
+++ src/main/java/kr/kro/deom/common/security/handler/OAuth2SuccessHandler.java
@@ -25,9 +25,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final JwtUtil jwtUtil;
     private final StoreService storeService;
     private final UserService userService;
-    private static final String REDIRECT_URL = "http://localhost:5713";
-
-    // private static final String REDIRECT_URL = "https://staging.deom.kro.kr/signin/callback";
+     private static final String REDIRECT_URL = "https://staging.deom.kro.kr/signin/callback";
 
     @Override
     public void onAuthenticationSuccess(
